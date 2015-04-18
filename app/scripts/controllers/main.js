5d4c715402abf27765ab01a7bd0dac2cd94c00ff
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-18 12:10:47 +0200

    Initialisation de git.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beautyRedSpaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the beautyRedSpaApp
+ */
+angular.module('beautyRedSpaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    angular.element(document).ready(function(){
+      angular.element('.bxslider').bxSlider({
+        mode : 'fade',
+        auto:true
+      });
+    });
+  })
+;
