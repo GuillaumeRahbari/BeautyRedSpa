3f42723ab14bf459428b3f28f591c71fd0230114
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-04-27 10:34:25 +0200

    ajout de la page contact

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name beautyRedSpaApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the beautyRedSpaApp
+ */
+angular.module('beautyRedSpaApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
